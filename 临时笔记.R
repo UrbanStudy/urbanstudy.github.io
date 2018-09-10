dfa1d37c133eae69df15c13fe7f63d9b2902b90d
qushen26 <qushen26+qushen@pdx.edu>
2018-09-10 14:28:22 -0700

    09-10

---
+++ 临时笔记.R
@@ -0,0 +1,18 @@
+A websit sample
+https://datacarpentry.org/R-ecology-lesson/index.html
+
+read_xls("")
+ 
+ quandl.com
+ 
+ federal Rever
+ 
+city-data.com
+
+wget
+
+curl::
+  
+  https://movement.uber.com/explore/boston/travel-times/query?lang=en-US&lat.=42.3584308&lng.=-71.0597732&z.=12&si=1116&ti=&ag=censustracts&dt[tpb]=ALL_DAY&dt[wd;]=1,2,3,4,5,6,7&dt[dr][sd]=2018-01-01&dt[dr][ed]=2018-01-31&cd=&sa;=&sdn=
+  
+  https://movement.uber.com/explore/san_francisco/travel-times/query?lang=en-US&lat.=37.7749295&lng.=-122.4194155&z.=12&si=1277&ti=&ag=censustracts&dt[tpb]=ALL_DAY&dt[wd;]=1,2,3,4,5,6,7&dt[dr][sd]=2018-03-01&dt[dr][ed]=2018-03-31&cd=&sa;=&sdn=
